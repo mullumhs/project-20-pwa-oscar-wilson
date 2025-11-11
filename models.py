55a751a94cb0b0777e2edbef45ec7332d9e8d075
oscar-wilson <oscar.wilson8@education.nsw.gov.au>
2025-11-12 10:40:44 +1100

    Started Task, Created Element class
    * Installed flask, flask-sqlalchemy
    * Added columns for element class
    
    To do:
    * Extra radioactivity information in the table

--- models.py
+++ models.py
@@ -4,3 +4,13 @@ db = SQLAlchemy()
 
 # Define your database model here
 # Example: class Item(db.Model):
+
+class Element(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(100), nullable=False)
+    symbol=db.Column(db.String(2), nullable=False)
+    group=db.Column(db.Integer, nullable=False)
+    period=db.Column(db.Integer, nullable=False)
+    melting_point=db.Column(db.Float) # in Kelvin at 1 atmosphere of pressure
+    boiling_point=db.Column(db.Float) # in Kelvin at 1 atmosphere of pressure
+    radioactive=db.Column(db.Boolean) # if all isotopes are radioactive
