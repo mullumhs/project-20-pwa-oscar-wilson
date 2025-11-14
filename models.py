d03bd2ebb8534cf5bc26e245cf3a68ff9327826a
oscar-wilson <oscar.wilson8@education.nsw.gov.au>
2025-11-14 12:39:05 +1100

    added extra radioactivity columns to element group
    most stable isotope and most stable half-life

--- models.py
+++ models.py
@@ -11,6 +11,9 @@ class Element(db.Model):
     symbol=db.Column(db.String(2), nullable=False)
     group=db.Column(db.Integer, nullable=False)
     period=db.Column(db.Integer, nullable=False)
+    metal=db.Column(db.Integer, nullable=False) # 0 is non-metal, 1 is semi-metal, 2 is metal, -1 is unsure (eg. for Og)?
     melting_point=db.Column(db.Float) # in Kelvin at 1 atmosphere of pressure
     boiling_point=db.Column(db.Float) # in Kelvin at 1 atmosphere of pressure
     radioactive=db.Column(db.Boolean) # if all isotopes are radioactive
+    most_stable_isotope_found=db.Column(db.Integer) # eg. 97 for Technetium
+    most_stable_halflife_found=db.Column(db.String(100)) # eg. 4.21±0.16 million years
