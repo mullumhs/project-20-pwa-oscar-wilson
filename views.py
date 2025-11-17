0f589db81ec7c422baeaef92e16f0f1a97036b22
oscar-wilson <oscar.wilson8@education.nsw.gov.au>
2025-11-17 14:32:56 +1100

    CRUD functionality started
    Read works
    Create has been started but does not work
    others have not been started

--- views.py
+++ views.py
@@ -1,5 +1,5 @@
 from flask import render_template, request, redirect, url_for, flash
-from models import db # Also import your database model here
+from models import db, Element
 
 # Define your routes inside the 'init_routes' function
 # Feel free to rename the routes and functions as you see fit
@@ -12,14 +12,31 @@ def init_routes(app):
     @app.route('/', methods=['GET'])
     def get_items():
         # This route should retrieve all items from the database and display them on the page.
-        return render_template('index.html', message='Displaying all items')
+        elements = Element.query.all()
+        return render_template('index.html', message='Displaying all items', elements=elements)
 
 
 
-    @app.route('/add', methods=['POST'])
+    @app.route('/add', methods=['GET', 'POST'])
     def create_item():
         # This route should handle adding a new item to the database.
-        return render_template('index.html', message='Item added successfully')
+        if request.method == 'POST':
+            new_element = Element(
+                name=request.form['name'],
+                symbol=request.form['symbol'],
+                group=request.form['group'],
+                period=request.form['period'],
+                metal=request.form['metal'],
+                melting_point=request.form['melting_point'],
+                boiling_point=request.form['boiling_point'],
+                radioactive=request.form['radioactive'],
+                most_stable_isotope_found=67,#request.form['most_stable_isotope_found'],
+                most_stable_halflife_found='69ms'#request.form['most_stable_halflife_found']
+            )
+            db.session.add(new_element)
+            db.session.commit()
+            return render_template('index.html', message='Item added successfully')
+        return render_template('index.html', message='Add?')
 
 
 
